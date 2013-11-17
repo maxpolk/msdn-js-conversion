fb149f590540c93ce902c4c6ee24a8c856e4dfeb
Max Polk <maxpolk@gmail.com>
2013-11-17 17:12:21 -0500

    Script to remove Function Method and Property suffix

---
+++ round-alice/FixLinks.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+#
+# Fix internal links within files according to the following rules:
+#
+#   Remove the suffix " Function", " Method", " Property"
+#
+echo "Fix internal links within files"
+for file in *.wiki; do
+    echo "    $file"
+    if [ "$file" = "upload-mapping.wiki" ]; then
+        echo "Ignoring non-content file"
+        continue
+    fi
+
+    #
+    # Change this:
+    #     [[Objects/Object/toString Method|toString method]]
+    # To this:
+    #     [[Objects/Object/toString|toString method]]
+    # Do the same for " Function", " Method", " Property"
+    #
+    sed -i -r -e "s/(\[\[.*) (Function|Method|Property)([]\|].*$)/\1\3/"
+done
