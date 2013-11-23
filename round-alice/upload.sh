13750582787a46da82529e0c49c839418a886981
maxpolk <maxpolk@gmail.com>
2013-11-22 21:47:50 -0500

    Changed path

--- round-alice/upload.sh
+++ round-alice/upload.sh
@@ -16,11 +16,11 @@ if [ -z "$WPD_API_ROOT" ]; then
 fi
 
 # Import path prefix
-PREFIX="Tests/javascript-a"
+PREFIX="Tests/javascript-b"
 
 grep '||' upload-mapping.wiki | sed 's/^| //' | while read line; do
     FILE="$(echo $line | sed 's/ .*$//')"
     PAGE="$(echo $line | sed 's/^.* || //')"
-    echo ../modified-wpd edit \""$PREFIX/$PAGE"\" \""$FILE"\"
-    ../modified-wpd edit "$PREFIX/$PAGE" "$FILE"
+    echo ../modified-wpd edit \""$PREFIX/$PAGE"\" \""$FILE"\" 'Script automated upload'
+    ../modified-wpd edit "$PREFIX/$PAGE" "$FILE" 'Script automated upload'
 done
