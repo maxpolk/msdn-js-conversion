7c6fa102415c48a19d78e5fbf4ee54be6c0a9c89
maxpolk <maxpolk@gmail.com>
2014-01-18 02:27:35 -0500

    Continuing work

--- round-cindy/section-rewrite.py
+++ round-cindy/section-rewrite.py
@@ -4,85 +4,76 @@ import os
 import sys
 import re
 
-#
-# REWRITE RULES
-#
+# True if we started a syntax section and need to finish it
+started_syntax_section = False
+finished_syntax_section = False
 
-# {{Page_Title}}
+def addTitleAndFlags (rewrite):
+    '''Add the top templates to the file, things like title and flags.'''
+    rewrite.write ("{{Page_Title}}\n")
+    rewrite.write ("{{Flags}}\n")
 
-# {{Flags}}
+def addSummary (rewrite, summary):
+    '''Add the summary section template, with given lines of contents.'''
+    rewrite.write ("{{Summary_Section|" + "\n".join (summary) + "}}\n")
 
-# THIS (first part of top unnamed section prior to code):
-# Provides support for creation of arrays of any data type.
-# BECOMES:
-# {{Summary_Section|Provides support for creation of arrays of any data type.}}
+def addSyntaxHeader (rewrite):
+    '''
+    Add the JS syntax template header.
+    Generate optional content with addSyntaxFormats and addSyntaxValues.
+    Finish by calling addsyntaxFooter.
+    '''
+    # Header
+    rewrite.write ("{{JS_Syntax")
 
-# THIS (leading space, top unnamed section):
-#  arrayObj = new Array()
-#  arrayObj = new Array([ size ])
-#  arrayObj = new Array([ element0 [, element1 [, ...[, elementN ]]]])
-# BECOMES (each line goes into JS_Syntax Formats / JS_Object_Format)
-# {{JS_Syntax
-# |Formats={{JS Object Format
-# |Format=arrayObj = new Array();
-# }}{{JS Object Format
-# |Format=arrayObj = new Array([ size ]);
-# }}{{JS Object Format
-# |Format=arrayObj = new Array([ element0 [, element1 [, ...[, elementN ]]]]);
-# }}
+def addSyntaxFormats (rewrite, syntax):
+    '''Add the JS syntax Format parameter, with given lines of contents.'''
+    # Fixed parameter
+    rewrite.write ("|Formats=")
+    # Repeated lines
+    for line in syntax:
+        rewrite.write ("{{JS_Object_Format\n")
+        rewrite.write ("|Format={}".format (line))
+        rewrite.write ("}}")
 
-# THIS (Parameters section, stuff beginning with ; PARAMETER: ):
-# ; arrayObj: Required. The variable name to which the '''Array''' object is assigned.
-# ; size: Optional. The size of the array. As arrays are zero-based, created elements will have indexes from zero to size -1.
-# ; element0,...,elementN: Optional. The elements to place in the array. This creates an array with n + 1 elements, and a '''length''' of n + 1. Using this syntax, you must supply more than one element.
-# BECOMES (each line goes into Values/ JS_Syntax_Parameter, split into Name, Required, Description):
-# |Values={{JS Syntax Parameter
-# |Name=arrayObj
-# |Required=Yes
-# |Description=The variable name to which the '''Array''' object is assigned.
-# }}{{JS Syntax Parameter
-# |Name=size
-# |Required=No
-# |Description=The size of the array. As arrays are zero-based, created elements will have indexes from zero to size -1.
-# }}{{JS Syntax Parameter
-# |Name=element0,...,elementN
-# |Required=No
-# |Description=The elements to place in the array. This creates an array with n + 1 elements, and a '''length''' of n + 1. Using this syntax, you must supply more than one element.
-# }}
+def addSyntaxValues (rewrite, syntax):
+    '''Add the JS syntax Values parameter, with given lines of contents.'''
+    # Fixed parameter
+    rewrite.write ("\n|Values=")
+    # Repeat for each line
+    for line in syntax:
+        #
+        # Split line into name, required, description.  Starts out like:
+        #     ; arrayObj: Required. The variable...is assigned.
+        # Ends up as:
+        #     name         arrayObj
+        #     required     Required
+        #     description  The variable...is assigned.
+        #
+        print "{} > [PARAMETER] {}".format ("?", line)
+        match = re.match ("^; ([^:]+): *(Optional|Required). *(.*)$", line)
+        if match:
+            name, required, description = match.group (1, 2, 3)
+            rewrite.write ("{{JS_Syntax_Parameter\n")
+            rewrite.write ("|Name={}\n".format (name))
+            rewrite.write ("|Required={}\n".format (required))
+            rewrite.write ("|Description={}".format (description))
+            rewrite.write ("}}")
+        elif (line is not ""):
+            print ("ERROR: could not parse syntax parameter:\n{}".format (line))
 
-# ALSO we close off the JS_Syntax whether or not we have a Parameters section:
-# }}
+def addSyntaxFooter (rewrite):
+    '''Finish off the syntax footer.'''
+    rewrite.write ("\n}}\n");
 
-# THIS (Examples section):
-# The following example illustrates the use of the '''0 . . .''' n properties of the '''arguments''' object. To fully understand the example, pass one or more arguments to the function:
-# 
-#  function ArgTest(){
-#     var s = "";
-#     s += "The individual arguments are: "
-#     for (n = 0; n &lt; arguments.length; n++){
-#        s += ArgTest.arguments[n] ;
-#        s += " ";
-#     }
-#     return(s);
-#  }
-#  document.write(ArgTest(1, 2, "hello", new Date()));
-# }}
-# BECOMES:
-# {{Examples_Section
-# |Examples=
-# The following example illustrates the use of the '''0 . . .''' n properties of the '''arguments''' object. To fully understand the example, pass one or more arguments to the function:
-# 
-#  function ArgTest(){
-#     var s = "";
-#     s += "The individual arguments are: "
-#     for (n = 0; n &lt; arguments.length; n++){
-#        s += ArgTest.arguments[n] ;
-#        s += " ";
-#     }
-#     return(s);
-#  }
-#  document.write(ArgTest(1, 2, "hello", new Date()));
-# }}
+def addExamples (rewrite, examples):
+    '''Add Examples_Section template instance.'''
+    # Fixed parameter
+    rewrite.write ("{{Examples_Section\n")
+    rewrite.write ("|Examples=" + "\n".join (examples))
+    for line in examples:
+        print "{} > [EXAMPLES] {}".format ("?", line)
+    rewrite.write ("}}\n")
 
 def processSection (filename, rewrite, section_name, section):
     '''
@@ -90,9 +81,16 @@ def processSection (filename, rewrite, section_name, section):
     of the named section (section_name: string) by writing to the
     output rewrite file (rewrite: open file handle).
     '''
+    global started_syntax_section, finished_syntax_section
+    # Finish off a previous template that has no more information to put into it
+    if (started_syntax_section and not finished_syntax_section and
+            section_name != "Parameters"):
+        addSyntaxFooter (rewrite)
+        finished_syntax_section = True
     # Determine if this is the top (unnamed) section
     if section_name == "":
         print "Unnamed top section contains {} lines:".format (len (section))
+        addTitleAndFlags (rewrite)
         #
         # Keep track of if we are in the summary section or code example
         # part of the top section.  We can tell because a space is the
@@ -100,18 +98,45 @@ def processSection (filename, rewrite, section_name, section):
         #
         past_summary = False
         count = 1
+        accum_summary = []
+        accum_code = []
         for line in section:
             if past_summary:
                 # We are in code example
                 print "{} > [CODE] {}".format (count, line)
+                accum_code.append (line)
             elif line.startswith (" "):
                 # Summary section finished, code example follows
                 past_summary = True
                 print "{} > [CODE] {}".format (count, line)
+                accum_code.append (line)
             else:
                 print "{} > [SUMMARY] {}".format (count, line)
-                rewrite.write ("{}\n".format (line))
+                accum_summary.append (line)
             count += 1
+        if (len (accum_summary) > 0):
+            addSummary (rewrite, accum_summary)
+        else:
+            print ("WARNING: no summary section")
+        if (len (accum_code) > 0):
+            # Remember to close off syntax section later
+            started_syntax_section = True
+            addSyntaxHeader (rewrite)
+            addSyntaxFormats (rewrite, accum_code)
+        else:
+            print ("WARNING: no code section")
+    elif (section_name == "Parameters"):
+        print "Section \"{}\" contains {} lines:".format (section_name, len (section))
+        if (not started_syntax_section):
+            print ("ERROR: we never started a syntax template, ignoring Parameters")
+        else:
+            # This concludes the Syntax template we started above
+            addSyntaxValues (rewrite, section)
+            addSyntaxFooter (rewrite)
+            finished_syntax_section = True
+    elif (section_name == "Example"):
+        print "Section \"{}\" contains {} lines:".format (section_name, len (section))
+        addExamples (rewrite, section)
     else:
         print "Section \"{}\" contains {} lines:".format (section_name, len (section))
         # Write section header
@@ -128,6 +153,10 @@ def processFile (filename):
     Read through a file, separating into mediawiki markup sections.
     Rewrite each section into a new file named with suffix ".rewrite".
     '''
+    global started_syntax_section, finished_syntax_section
+    started_syntax_section = False
+    finished_syntax_section = False
+
     print ("===== {} =====".format (filename))
     rewrite = open (filename + ".rewrite", 'w')
     section_name = ""
@@ -137,6 +166,9 @@ def processFile (filename):
             line = line.rstrip ("\n")
             if line.startswith ("="):
                 # Process section we finished accumulating
+                # Strip off empty lines from the end
+                while (len (section) > 0 and re.match ('^[ \t]*$', section[-1])):
+                    del section[-1]
                 if len (section) > 0:
                     processSection (filename, rewrite, section_name, section)
                 section_name = ""
@@ -145,13 +177,18 @@ def processFile (filename):
                 section_name = line.translate (None, '=')
             else:
                 section.append (line)
+    # Strip off empty lines from the end
+    while (len (section) > 0 and re.match ('^[ \t]*$', section[-1])):
+        del section[-1]
     # Process section we finished accumulating
     if len (section) > 0:
         processSection (filename, rewrite, section_name, section)
         rewrite.close ()
+    # See if things went well
+    if (started_syntax_section and not finished_syntax_section):
+        print ("ERROR: started syntax template but didn't finish it")
 
 # Read each .wiki file in the current directory
 for filename in os.listdir ("."):
     if filename.endswith (".wiki"):
-        if filename != 'upload-mapping.wiki':
-            processFile (filename)
+        processFile (filename)
