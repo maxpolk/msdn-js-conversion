54e57a11850270dface02bca23d73443c8d4ed4b
maxpolk <maxpolk@gmail.com>
2014-01-23 20:51:46 -0500

    Fixed external attribution in footer

--- round-cindy/section-rewrite.py
+++ round-cindy/section-rewrite.py
@@ -398,11 +398,12 @@ def addFooter (rewrite):
     rewrite.write ("{}\n".format ("{{Topics | JS Basic}}"))
 
     # Add an external attribution to MSDN
-    rewrite.write ("{}\n".format ('''{{External_Attribution
+    rewrite.write ("{}\n".format ('''
+{{External_Attribution
 |Is_CC-BY-SA=No
 |Sources=MSDN
 |MDN_link=
-|MSDN_link=http://msdn.microsoft.com/en-us/library/ie/yek4tbz0%28v=vs.94%29.aspx
+|MSDN_link=http://msdn.microsoft.com/en-us/library/ie/yek4tbz0%28v=vs.94%29.aspx Windows Internet Explorer JavaScript reference
 |HTML5Rocks_link=
 }}'''))
 
